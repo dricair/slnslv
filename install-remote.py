3c69f890240f065e3478f96b2939921a0edfce4e
Cédric Airaud <cairaud@gmail.com>
2017-08-31 21:07:52 +0200

    Fix script (partial)

--- install-remote.py
+++ install-remote.py
@@ -1,49 +1,38 @@
 #! /usr/bin/python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 import sys
 import os
 import os.path
 import subprocess
 import tempfile
-import re
-import ftplib
-import httplib
-import hashlib
-import urllib
-import optparse
-import smtplib
-import socket
 import logging
+import argparse
 
-from email.MIMEText import MIMEText
+parser = argparse.ArgumentParser()
+parser.add_argument("--email", dest="email", default="", help="Email to send log message")
+parser.add_argument("--debug", dest="debug", default=False, action="store_true", help="Activate debug messages")
+parser.add_argument("--test",  dest="test",  default=False, action="store_true", help="Do not execute commands")
 
-parser = optparse.OptionParser()
-parser.add_option("-e", "--email", dest="email", default="", type="string",
-                  help="Email to send log message")
+args = parser.parse_args()
 
-(options, args) = parser.parse_args()
-
-print __file__
-
-updateFtp = True
-updateSql = False
+log_level = logging.INFO
+if args.debug: log_level = logging.DEBUG
 
 logger = logging.getLogger("install")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(log_level)
 
 fh = logging.FileHandler('mail.log')
-fh.setLevel(logging.DEBUG)
+fh.setLevel(log_level)
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(log_level)
 
-fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s  %(levelname)-10s - %(message)s'))
-ch.setFormatter(logging.Formatter('%(name)s %(levelname)-10s %(message)s'))
+fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)-10s - %(message)s'))
+ch.setFormatter(logging.Formatter('%(levelname)-10s %(message)s'))
 
 logger.addHandler(ch)
 logger.addHandler(fh)
-
 
 server = "ftp.stadelaurentinnatation.fr"
 username = "stadelau"
@@ -54,48 +43,40 @@ localsql = os.path.join(localdir, sqlfile)
 remotesite = "www.stadelaurentinnatation.fr"
 remotedir = "/www/register" # Beware not ending with /
 excludes = [".git/", ".gitignore", "TODO", "README", os.path.basename(__file__), "mail.log",  "composer.*", 
-            "app/config/parameters.yml", "app/cache/", "app/logs/"]
+            "app/config/parameters.yml", "app/cache/", "app/logs/", "*.swp", "*~"]
 lftp = "/usr/bin/lftp"
-updatepage = "/update-db.php"
-parameters_remote = "app/config/parameters-remote.yml"
 ftp = "ftp://%s:%s@%s" % (username, password, server)
 
 def run_command(cmd, logger):
-  (stdout, stderr) = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE).communicate()
+  p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  (stdout, stderr) = ("", "")
+  if not args.test:
+    (stdout, stderr) = p.communicate()
+
+  if type(stdout) is bytes: stdout = stdout.decode("utf-8")
+  if type(stderr) is bytes: stderr = stderr.decode("utf-8")
 
   logger.debug("Command: " + subprocess.list2cmdline(cmd))
-  logger.debug(stdout,)
-  if (stderr != ""): logger.error(stderr)
+  logger.debug(stdout)
+  if (stderr != ""): 
+      if p.returncode:
+          logger.error(stderr)
+      else:
+          logger.warning(stderr)
 
+def synchronize(directory):
+    (ldir, rdir) = (os.path.join(localdir, directory), os.path.join(remotedir, directory))
+    logger.info("FTP synchronisation...")
+    logger.info("  %s -> %s on %s@%s" % (ldir, rdir, username, server))
 
-# Synchronize FTP
-if updateFtp:
-  logger.info("FTP synchronisation...")
-  logger.info("  %s -> %s on %s@%s" % (localdir, remotedir, username, server))
-  commands = "mirror --no-symlinks --use-cache -e -R %s %s %s; quit" % (" ".join(map(lambda x: "--exclude %s" % x, excludes)),
-                                                                        localdir, remotedir)
-  run_command([lftp, ftp, "-e", commands], logger)
+    commands = "mirror --no-symlinks --use-cache -e -R %s %s %s; quit" % (" ".join(map(lambda x: "--exclude %s" % x, excludes)),
+                                                                          ldir, rdir)
+    run_command([lftp, ftp, "-e", commands], logger)
 
-  logger.info("Répertoires logs et cache")
-  commands = "cd %s; rm -rf logs; rm -rf cache; mkdir logs; mkdir cache; chmod 777 logs; chmod 777 cache; quit" % os.path.join(remotedir, 'app')
-  run_command([lftp, ftp, "-e", commands], logger)
+synchronize("src/SLN/RegisterBundle")
 
-  logger.info("Fichier parameters.yml (From %s)" % parameters_remote)
-  commands = "cd %s; put -O app/config %s -o parameters.yml; quit" % (remotedir, parameters_remote)
-  run_command([lftp, ftp, "-e", commands], logger)
-
-
-# Change DB file (replace)
-if updateSql:
-  logger.info("SQL DB replace...")
-  sqlin = open(localsql, "r")
-  sqlout = tempfile.NamedTemporaryFile(mode="w+", suffix=".sql", delete=False)
-  msg("Tmp file: " + sqlout.name)
-
-  for line in sqlin.readlines():
-      sqlout.write(line.replace("solidarsport.free.fr", "www.solidarsport.fr"))
-  sqlout.close()
-
+logger.info("Répertoires logs et cache")
+commands = "cd %s; rm -rf logs; rm -rf cache; mkdir logs; mkdir cache; chmod 777 logs; chmod 777 cache; quit" % os.path.join(remotedir, 'app')
+run_command([lftp, ftp, "-e", commands], logger)
 
 
